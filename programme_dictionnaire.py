18315d4ca4835cad10420e405aa7becb7b283c06
root <root@LAPTOP-VIGHRDQ9>
2023-10-07 17:18:28 +0200

    Optimisation du code en utilisant un dictionnaire

---
+++ programme_dictionnaire.py
@@ -0,0 +1,68 @@
+#Lien du sujet : https://www.codingame.com/ide/puzzle/don't-panic-episode-1
+
+'''
+Méthode de résolution :
+    - 1/ Optimale : Utilisation d'un dictionnaire
+    - 2/ Brute Force : Utilisation d'une liste
+'''
+
+nb_floors, width, nb_rounds, exit_floor, exit_pos, nb_total_clones, nb_additional_elevators, nb_elevators = [int(i) for i in input().split()]
+elevator_floor, elevator_pos = exit_floor,exit_pos #Initialisation avec l'exit - Cas où il n'y a qu'un étage avec une sortie
+dict_floor_pos = {exit_floor:exit_pos}  #Dictionnaire de couple étage/sortie, Ajout du couple pour l'ascenseur exit
+
+for i in range(nb_elevators): #Remplissage de la liste couple étage/sortie
+    elevator_floor, elevator_pos = [int(j) for j in input().split()]
+    dict_floor_pos[elevator_floor] = elevator_pos
+
+while True:
+    inputs = input().split()
+    clone_floor, clone_pos = int(inputs[0]),int(inputs[1]) #Etage et Position du clone
+    direction = inputs[2]  # Direction du clone learder: LEFT or RIGHT
+
+    if direction == "NONE":
+        print("WAIT")
+        continue
+
+    elevator_pos = dict_floor_pos[clone_floor]
+    dist_asc_clone = elevator_pos - clone_pos
+
+    #Si dist_asc_clone > 0 : l'ascenseur est positionné à droite du clone
+    #Si la direction du clone est "LEFT" : il faut que le premier clone s'arrête car il est dans le mauvais sens
+    if (((dist_asc_clone > 0) and (direction=="LEFT")) or 
+    ((dist_asc_clone < 0) and (direction=="RIGHT"))):
+        print("BLOCK")
+    else:
+        print("WAIT")
+
+
+"""
+#VERSION AVEC LES LISTES
+
+nb_floors, width, nb_rounds, exit_floor, exit_pos, nb_total_clones, nb_additional_elevators, nb_elevators = [int(i) for i in input().split()]
+elevator_floor, elevator_pos = exit_floor,exit_pos #Initialisation avec l'exit - Cas où il n'y a qu'un étage avec une sortie
+L_elevator_floor_and_pos = [[exit_floor,exit_pos]] #Liste de couple étage/sortie, Ajout du couple pour l'ascenseur exit
+
+for i in range(nb_elevators): #Remplissage de la liste couple étage/sortie
+    elevator_floor, elevator_pos = [int(j) for j in input().split()]
+    L_elevator_floor_and_pos.append([elevator_floor, elevator_pos])
+
+while True:
+    inputs = input().split()
+    clone_floor, clone_pos = int(inputs[0]),int(inputs[1]) #Etage et Position du clone
+    direction = inputs[2]  # direction of the leading clone: LEFT or RIGHT
+
+    for couple in L_elevator_floor_and_pos: #trouver la position de l'ascenseur
+        if couple[0] == clone_floor:
+            elevator_pos = couple[1]
+            break
+
+    dist_asc_clone = elevator_pos - clone_pos
+
+    #Si dist_asc_clone > 0 : l'ascenseur est positionné à droite du clone
+    #Si la direction du clone est "LEFT" : il faut que le premier clone s'arrête car il est dans le mauvais sens
+    if (((dist_asc_clone > 0) and (direction=="LEFT")) or 
+    ((dist_asc_clone < 0) and (direction=="RIGHT"))):
+        print("BLOCK")
+    else:
+        print("WAIT")
+"""
